3cf6008f47ee3ecc72a3d627bdfc2e3da2700dc1
Maharavo1 <hei.maharavo@gmail.com>
2026-08-20 17:47:24 +0200

    fix: Update auth

--- app/core/security.py
+++ app/core/security.py
@@ -8,22 +8,20 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
 def create_access_token(user_id: int, role: str):
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    playload = {
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
         "sub": str(user_id),
         "role": role,
-        "exp": expire
+        "exp": expire,
     }
-
-    return jwt.encode(playload, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_access_token(token: str):
     try:
-        playload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return playload
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
     except jwt.ExpiredSignatureError:
         print("[DEBUG] Le token a expiré !")
         return None
--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?client_encoding=utf8"
 
 engine = create_engine(DATABASE_URL)
 
